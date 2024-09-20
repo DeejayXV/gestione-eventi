e53a57d492c5ce61c4b9dc9e6a44df69c557bf94
John <deejay.gvr@gmail.com>
2024-09-20 11:39:41 +0200

    Initial commit

---
+++ src/main/java/com/gestione_eventi/model/User.java
@@ -0,0 +1,26 @@
+package com.gestione_eventi.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    // Costruttori, getter e setter
+}
